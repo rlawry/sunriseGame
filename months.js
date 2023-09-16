b2d364eadc62612f2b1af86a28f187cf4b48f9aa
Galabeam <68124962+Galabeam@users.noreply.github.com>
2023-09-16 11:16:06 -0400

    made code neat, fixed .md class ~= .html class

--- months.js
+++ months.js
@@ -25,9 +25,9 @@ var winCount = 0;
 var newCreated = false;
 
 function makeMonth(){
-    if(newCreated==false){
-        while(previousMonth == monthNumber){
-            monthNumber = Math.floor(Math.random()*(monthNames.length));
+    if (newCreated == false) {
+        while (previousMonth == monthNumber) {
+            monthNumber = Math.floor(Math.random() *( monthNames.length));
         }
         var monthObject = document.getElementById("month");
         monthObject.innerHTML = monthNames[monthNumber][0];
@@ -35,30 +35,30 @@ function makeMonth(){
         newCreated = true;
         document.getElementById("answer").innerHTML = "Pick the direction!";
     }
-    else if(newCreated == true){
+    else if (newCreated == true) {
         document.getElementById("answer").innerHTML = "Cheater.  Pick a direction first.";
     }
 }
 
-function checkDirection(e){
-    if(newCreated == true){
-        if(e.value==monthNames[monthNumber][1]){
+function checkDirection(e) {
+    if (newCreated == true) {
+        if (e.value == monthNames[monthNumber][1]) {
             document.getElementById("answer").innerHTML = "Correct";
             document.getElementById("answer").classList.remove("lose-class");
             newCreated = false;
             winCount++;
-            if(handicap>5&&monthNames[monthNumber][1]==1){
+            if (handicap>5&&monthNames[monthNumber][1] == 1) {
                 monthNames = monthNames.slice(0,baseNames.length);
                 document.getElementById("answer").innerHTML = "Finally correct! You missed this one " + handicap + " times.";
                 handicap = 0;
             }
         }
-        else if(e.value!=monthNames[monthNumber][1]){
+        else if (e.value!=monthNames[monthNumber][1]) {
             document.getElementById("answer").innerHTML = "No points. Go to the next month.";
             document.getElementById("answer").classList.add("lose-class");
             newCreated = false;
-            if(winCount>0){winCount--;}
-            if(monthNames[monthNumber][1]==1){
+            if (winCount>0){winCount--;}
+            if (monthNames[monthNumber][1]==1) {
                 monthNames.push(handicap1);
                 monthNames.push(handicap2);
                 handicap++;
@@ -66,7 +66,7 @@ function checkDirection(e){
         }
         document.getElementById("points").innerHTML = winCount;
     }
-    else if(newCreated == false){
+    else if (newCreated == false) {
         document.getElementById("answer").innerHTML = "You already guessed. Move to the next month.";
     }
     console.log(handicap);
